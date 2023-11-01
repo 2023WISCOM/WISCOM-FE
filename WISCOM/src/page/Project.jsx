08e69d16ae510012225bc1ebce4c07d6e6ab3c77
또이 <90364711+imddoy@users.noreply.github.com>
2023-11-01 15:25:43 +0900

    Revert "Feat: 색상 랜덤 구현"

--- WISCOM/src/page/Project.jsx
+++ WISCOM/src/page/Project.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
+
 import WorkInfo from '../components/Project/WorkInfo';
 import IntroWork from '../components/Project/IntroWork';
 import Developer from '../components/Project/Developer';
-import Likes from '../components/Project/Likes';
-import Comment from '../components/Project/Comment';
 
 export default function Project() {
   return (
@@ -11,8 +10,6 @@ export default function Project() {
       <WorkInfo />
       <IntroWork />
       <Developer />
-      <Likes />
-      <Comment />
     </ProjectWrapper>
   );
 }
@@ -21,8 +18,4 @@ const ProjectWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-
-  @media (max-width: 320px) {
-    display: none;
-  }
 `;
