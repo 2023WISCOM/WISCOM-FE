b8a5fa2900a7741d66d758e23082844857257332
ose0221 <ose0221@naver.com>
2023-11-03 19:24:26 +0900

    좋아요 이미지 변경

--- WISCOM/src/components/Project/Likes.jsx
+++ WISCOM/src/components/Project/Likes.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
-import likeImg from '../../img/Project/likes.svg';
+import unclickedLikeImg from '../../img/Project/Likes.svg';
+import clickedLikeImg from '../../img/Project/ClickedLikes.svg';
 import * as L from './LikesStyle';
 
 const Likes = () => {
   const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   const handleLikeClick = () => {
-    setLikes(likes + 1);
+    //setLikes(likes + 1);
+    if (isLiked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+    setIsLiked(!isLiked);
   };
 
   return (
-    <L.LikesWrapper>
-      <L.LikesImg src={likeImg} alt="image" onClick={handleLikeClick} />
+    <L.LikesWrapper onClick={handleLikeClick}>
+      <L.LikesImg src={isLiked ? clickedLikeImg : unclickedLikeImg} alt="image" />
       <L.LikesSpan>{likes}</L.LikesSpan>
     </L.LikesWrapper>
   );
